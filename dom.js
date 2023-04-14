6682407c845bac641c52019a56f028838633702f
ShivanginiRathore <shivangini.atcs@gmail.com>
2023-04-14 13:02:40 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,6 @@
+const header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px black';
+
+const title = document.getElementsByClassName('title');
+title[0].innerHTML = '<b>Add Items</b>'
+title[0].style.color = 'green'
