352ff611e42da79fbe0d86417c840c8214a2059a
ShivanginiRathore <shivangini.atcs@gmail.com>
2023-04-14 13:58:05 +0530

    DOM-Q4

--- dom.js
+++ dom.js
@@ -2,5 +2,9 @@ const header = document.getElementById('main-header');
 header.style.borderBottom = 'solid 3px black';
 
 const title = document.getElementsByClassName('title');
-title[0].innerHTML = '<b>Add Items</b>'
+title[0].style.fontWeight = 'bold'
 title[0].style.color = 'green'
+
+const ul = document.getElementById('items');
+ul.children[2].style.background = 'green'
+ul.style.fontWeight= 'bold'
