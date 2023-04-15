1bcd503146a28cbddfe9f6468ac122fa89e39c30
ShivanginiRathore <shivangini.atcs@gmail.com>
2023-04-15 15:24:21 +0530

    Add_Desc&Filter

--- Item/addItem.js
+++ Item/addItem.js
@@ -1,14 +1,19 @@
 const form = document.getElementById('addForm');
 const itemList = document.getElementById('items');
+const filter = document.getElementById('filter');
+
 
 form.addEventListener('submit', addItem);
 
 itemList.addEventListener('click', removeItem);
 
+filter.addEventListener('keyup',filterItems);
+
 function addItem(e){
     // console.log(1);
     e.preventDefault();
     const input = document.getElementById('item').value;
+    const desc = document.getElementById('itemDesc').value;
     
     const newLi = document.createElement('li');
     newLi.className = 'list-group-item';
@@ -26,6 +31,9 @@ function addItem(e){
     editBtn.appendChild(document.createTextNode('Edit'));
     newLi.appendChild(editBtn);
 
+    // ADD DESCRIPTION
+    newLi.appendChild(document.createTextNode(` ${desc}`));
+
     itemList.appendChild(newLi);
 
     // CODE BY ME
@@ -40,4 +48,37 @@ function removeItem(e){
             var li = e.target.parentElement;
             itemList.removeChild(li);        }
     }
+}
+
+function filterItems(e){
+    let text = e.target.value.toLowerCase();
+    let items = itemList.getElementsByTagName('li');
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        var desc = item.lastChild.textContent;
+        console.log(itemName)
+        if(itemName.toLowerCase().indexOf(text) != -1 || desc.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
+        } else{
+            item.style.display = 'none';
+        }
+    })
+
+
+    // My Code
+
+    // for(let i=0; i<items.length; i++){
+    //     var itemName = items[0].firstChild.textContent;
+
+    //     console.log('Item from list is -> '+itemName+': Text is ->'+text);
+    //     if(itemName.toLowerCase().indexOf(text) != -1){
+    //         items[0].style.display = 'block';
+    //     } else{
+    //         items[0].style.display = 'none';
+    //     }
+        
+    // }
+
+
+
 }
